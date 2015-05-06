2d78ba87560cd39ae52290609a112a31af4de5d3
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-05-05 22:47:57 -0300

    Creating and implementing tableview to list companies

---
+++ BestOffer/BestOffer/MarketListViewController.m
@@ -0,0 +1,47 @@
+//
+//  ViewController.m
+//  BestOffer
+//
+//  Created by Jhonathan Wyterlin on 04/05/15.
+//  Copyright (c) 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "MarketListViewController.h"
+
+@interface MarketListViewController()<UITableViewDataSource,UITableViewDelegate>
+
+@property(nonatomic,strong) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation MarketListViewController
+
+#pragma mark - View Lifecycle
+
+-(void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+-(void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UITableViewDatasource methods
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [UITableViewCell new];
+    
+    return cell;
+    
+}
+
+#pragma mark - UITableViewDelegate methods
+
+@end
