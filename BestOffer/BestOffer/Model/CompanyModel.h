efabc8767c88eb587dff85950fd077d65ef0f3ca
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-05-06 22:46:33 -0300

    Created Company model

---
+++ BestOffer/BestOffer/Model/CompanyModel.h
@@ -0,0 +1,13 @@
+//
+//  CompanyModel.h
+//  BestOffer
+//
+//  Created by Jhonathan Wyterlin on 06/05/15.
+//  Copyright (c) 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CompanyModel : NSObject
+
+@end
---
+++ BestOffer/BestOffer/Model/CompanyModel.m
@@ -0,0 +1,13 @@
+//
+//  CompanyModel.m
+//  BestOffer
+//
+//  Created by Jhonathan Wyterlin on 06/05/15.
+//  Copyright (c) 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "CompanyModel.h"
+
+@implementation CompanyModel
+
+@end
