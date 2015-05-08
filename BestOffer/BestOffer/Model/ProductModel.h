0a0430cbbe07ec6eb30d3f0b3ce46a6487e21cef
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-05-07 23:02:31 -0300

    Created Product model

---
+++ BestOffer/BestOffer/Model/ProductModel.h
@@ -0,0 +1,13 @@
+//
+//  ProductModel.h
+//  BestOffer
+//
+//  Created by Jhonathan Wyterlin on 07/05/15.
+//  Copyright (c) 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ProductModel : NSObject
+
+@end
---
+++ BestOffer/BestOffer/Model/ProductModel.m
@@ -0,0 +1,13 @@
+//
+//  ProductModel.m
+//  BestOffer
+//
+//  Created by Jhonathan Wyterlin on 07/05/15.
+//  Copyright (c) 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "ProductModel.h"
+
+@implementation ProductModel
+
+@end
