f345b86f71bdabdaef58b22b062be2d4e273511f
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-09-25 22:18:54 -0300

    Created ProductListViewController file

---
+++ BestOffer/BestOffer/Controller/ProductListViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProductListViewController.h
+//  BestOffer
+//
+//  Created by Jhonathan Wyterlin on 25/09/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "GenericViewController.h"
+
+@interface ProductListViewController : GenericViewController
+
+@end
---
+++ BestOffer/BestOffer/Controller/ProductListViewController.m
@@ -0,0 +1,25 @@
+//
+//  ProductListViewController.m
+//  BestOffer
+//
+//  Created by Jhonathan Wyterlin on 25/09/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "ProductListViewController.h"
+
+@interface ProductListViewController ()
+
+@end
+
+@implementation ProductListViewController
+
+-(void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+-(void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
