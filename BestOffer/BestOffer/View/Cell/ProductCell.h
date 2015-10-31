ad16ccb22efda4623db35be50d0f98d4e4cb27b8
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-10-30 23:53:18 -0200

    Created ProductCell file

---
+++ BestOffer/BestOffer/View/Cell/ProductCell.h
@@ -0,0 +1,13 @@
+//
+//  ProductCell.h
+//  BestOffer
+//
+//  Created by Jhonathan Wyterlin on 30/10/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProductCell : UITableViewCell
+
+@end
---
+++ BestOffer/BestOffer/View/Cell/ProductCell.m
@@ -0,0 +1,23 @@
+//
+//  ProductCell.m
+//  BestOffer
+//
+//  Created by Jhonathan Wyterlin on 30/10/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import "ProductCell.h"
+
+@implementation ProductCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
